350283800846fd1577dc25e905e7a8372d336c40
George Townley <george@knowlesavenue.com>
2023-12-20 14:09:53 +0000

    update table formatting

--- src/components/CryptoTable.js
+++ src/components/CryptoTable.js
@@ -5,22 +5,48 @@ const CryptoTable = () => {
   const API_KEY = process.env.REACT_APP_COINLAYER_API_KEY;
 
   useEffect(() => {
+    // Define the top 20 cryptocurrencies by their symbols
+    // Update this list to your preference of top 20 cryptocurrencies
+    const topCryptos = [
+      "BTC",
+      "ETH",
+      "XRP",
+      "BCH",
+      "LTC",
+      "ADA",
+      "DOT",
+      "LINK",
+      "BNB",
+      "XLM",
+      "USDT",
+      "DOGE",
+      "UNI",
+      "AAVE",
+      "SOL",
+      "EOS",
+      "FTT",
+      "TRX",
+      "FIL",
+      "XTZ",
+    ];
+
     // Fetch data from Coinlayer
     fetch(`http://api.coinlayer.com/api/live?access_key=${API_KEY}`)
       .then((response) => response.json())
       .then((data) => {
-        // Assuming the API returns an array of cryptocurrencies
-        // Extract the top 20 tokens and relevant information
-        const top20Tokens = Object.keys(data.rates)
-          .slice(0, 20)
-          .map((key) => {
-            return {
-              name: key,
-              rate: data.rates[key],
-              // Add other relevant data fields here as needed
-            };
-          });
-        setCryptoData(top20Tokens);
+        // Filter out the top 20 tokens from the data
+        const filteredData = topCryptos.map((crypto) => {
+          // Use toFixed to format the rate to two decimal places
+          // Parse the rate to a float in case it's returned as a string
+          const rate = parseFloat(data.rates[crypto]);
+          return {
+            name: crypto,
+            rate: rate ? rate.toFixed(2) : "Not available",
+          };
+        });
+
+        // Update state with the filtered data
+        setCryptoData(filteredData);
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
@@ -38,8 +64,8 @@ const CryptoTable = () => {
           </tr>
         </thead>
         <tbody>
-          {cryptoData.map((token) => (
-            <tr key={token.name}>
+          {cryptoData.map((token, index) => (
+            <tr key={index}>
               <td>{token.name}</td>
               <td>{token.rate}</td>
               {/* Add additional data points here */}
