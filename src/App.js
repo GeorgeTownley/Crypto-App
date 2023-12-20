8867a0bbac1279a4b7d4f0026669be3bfe9db0a3
George Townley <george@knowlesavenue.com>
2023-12-20 13:46:49 +0000

    remove broken modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import CryptoTable from "./components/CryptoTable";
-import Modal from "./components/Modal";
 
 function App() {
   return (
     <div className="App">
-      <div className="crypto-glow"></div>{" "}
-      {/* This will create the glowing effect */}
       <Navbar />
-      <Modal />
       <div className="crypto-table-container">
         <CryptoTable />
       </div>
